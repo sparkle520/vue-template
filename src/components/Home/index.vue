597632a68d814f66f89c5becebb158ab5798053f
sparkle520 <949784350@qq.com>
2024-11-30 20:39:22 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   README.md
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.vue
    	new file:   src/assets/css/index.css
    	new file:   src/assets/css/normalize.css
    	new file:   src/assets/store/theme.ts
    	deleted:    src/components/HelloWorld.vue
    	new file:   src/components/Home/index.vue
    	modified:   src/main.ts
    	new file:   src/router/index.ts
    	deleted:    src/style.css
    	new file:   uno.config.ts
    	modified:   vite.config.ts

---
+++ src/components/Home/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Welcome to Stay</h1>
+        <div class="container">
+            <div class="">
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
